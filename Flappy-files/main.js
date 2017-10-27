a7612e0cdedb59d2ac88a246b7347cfed686360e
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-10-27 14:53:42 -0400

    added pipes attempted to figure out error

--- Flappy-files/main.js
+++ Flappy-files/main.js
@@ -2,6 +2,7 @@ var mainState = {
   //Will execute at the beginning
   preload: function () {
       game.load.image('bird', 'assets/bird.png');
+      game.load.image('pipe', 'assets/pipe.png');
   },
 
   //Called after preload
@@ -24,9 +25,12 @@ var mainState = {
       this.bird.body.gravity.y = 1000;
 
       //Call 'jump' function when spaceKey is pressed down
-      var spaceKey = game.input.keyboard.addkey(
-                      Phaser.Keyboard.SPACEBAR);
-      spaceKey.onDown.add(this.jump, this);
+      // var spaceKey = game.input.keyboard.addkey(
+      //                 Phaser.Keyboard.SPACEBAR);
+      // spaceKey.onDown.add(this.jump, this);
+      //
+      // //Create an empty group
+      // this.pipes = game.add.group();
   },
 
   //Contains game logic
@@ -44,8 +48,36 @@ var mainState = {
 
   restartGame: function () {
     game.state.start('main');
-  }
+  },
 
+  //Function to add A pipe taking x and y as params
+  addOnePipe: function(x, y) {
+      //Create pipe at the x and y position
+      var pipe = game.add.sprite(x, y, 'pipe');
+
+      //Add pipe to the pipe group
+      this.pipes.add(pipe);
+
+      //Enable physics to the pipe
+      game.physics.arcade.enable(pipe);
+
+      //Add velocity to pipe to make it move left
+      pipe.body.velocity.x = -200;
+
+      //Automatically 'kill' pipe when it is no longer visible
+      pipe.checkWorldBounds = true;
+      pipe.outOfBoundsKill = true;
+  },
+
+  addRowOfPipes: function() {
+      //Randomly choose number between 1 and 5
+      //Chosen number will the the "hole" position
+      var hole = Math.floor(Math.random() * 5) + 1;
+
+      for(var i = 0; i < 8; i++)
+          if(i != hole && i != hole + 1)
+            this.addOnePipe(400, i * 60 + 10);
+  },
 };
 
 //Initializes Phaser         |
