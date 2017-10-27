aec7e6b5d58c5961551b72d3bb0e65e446cdf3f2
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-10-27 12:46:01 -0400

    made correction, added bird sprite to proload, and added previous preload code into the create function

--- Flappy-files/main.js
+++ Flappy-files/main.js
@@ -1,32 +1,32 @@
 var mainState = {
   //Will execute at the beginning
   preload: function () {
-    //Set the backgroundColor to blue
-    game.stage.backgroundColor = '#72c5cf';
-
-    //Set physics
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-
-    //Display bird at position x=100 & y=245
-    //                              |
-    this.bird = game.add.sprite(100, 245, 'bird');
-
-    //Add physics to the bird
-    //Physics give the ability for: movements, gravity, collisions, etc.
-    game.physics.arcade.enable(this.bird);
-
-    // Enable gravity to the bird so it falls on the y-axis
-    this.bird.body.gravity.y = 1000;
-
-    //Call 'jump' function when spaceKey is pressed down
-    var spaceKey = game.input.keyboard.addkey(
-                    Phaser.keyboard.SPACEBAR);
-    spaceKey.onDown.add(this.jump, this);
+      game.load.image('bird', 'assets/bird.png');
   },
 
   //Called after preload
   create: function() {
+      //Set the backgroundColor to blue
+      game.stage.backgroundColor = '#72c5cf';
 
+      //Set physics
+      game.physics.startSystem(Phaser.Physics.ARCADE);
+
+      //Display bird at position x=100 & y=245
+      //                              |
+      this.bird = game.add.sprite(100, 245, 'bird');
+
+      //Add physics to the bird
+      //Physics give the ability for: movements, gravity, collisions, etc.
+      game.physics.arcade.enable(this.bird);
+
+      // Enable gravity to the bird so it falls on the y-axis
+      this.bird.body.gravity.y = 1000;
+
+      //Call 'jump' function when spaceKey is pressed down
+      var spaceKey = game.input.keyboard.addkey(
+                      Phaser.keyboard.SPACEBAR);
+      spaceKey.onDown.add(this.jump, this);
   },
 
   //Contains game logic
